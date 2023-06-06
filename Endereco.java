9fd2eb343d32d23ce40337d9e573ddeb02bd3c31
Natan Pivetta <51028072+NatanPivetta@users.noreply.github.com>
2023-06-06 16:48:23 -0300

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,78 @@
+package dominio;
+
+public class Endereco {
+    private String nomeDaRua;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public Endereco(String rua, String cid, String est, String pais, String cep, String num, String comp) {
+        this.nomeDaRua = rua;
+        this.cidade = cid;
+        this.estado = est;
+        this.pais = pais;
+        this.cep = cep;
+        this.numero = num;
+        this.complemento = comp;
+    }
+
+
+
+    public void setNomeDaRua(String rua){
+        this.nomeDaRua = rua;
+    }
+
+    public String getNomeDaRua(){
+        return this.nomeDaRua;
+    }
+
+
+    public void setCidade(String cid){
+        this.cidade = cid;
+    }
+
+    public String getCidade(){
+        return this.cidade;
+    }
+    public void setEstado(String est){
+        this.estado = est;
+    }
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public void setPais(String x){
+        this.pais = x;
+    }
+    public String getPais(){
+        return this.pais;
+    }
+
+    public void setCep(String x){
+        this.cep = x;
+    }
+    public String getCep(){
+        return this.cep;
+    }
+
+    public void setNumero(String x){
+        this.numero = x;
+    }
+    public String getNumero(){
+        return this.numero;
+    }
+
+
+    public void setComplemento(String x){
+        this.complemento = x;
+    }
+    public String getComplemento(){
+        return this.complemento;
+    }
+
+
+}
+
